723aa7fb6635373c95499ff07d53bca6b0c7eded
NikitaKhadnevich <vadim-swich@yandex.ru>
2021-07-20 18:52:30 +0300

    Fixed Api  file

--- Freedom/less12(AGE)/src/components/Api/Api.js
+++ Freedom/less12(AGE)/src/components/Api/Api.js
@@ -1,6 +1,6 @@
 export const baseUrl = 'https://age-of-empires-2-api.herokuapp.com/api/v1'
 // export const proxy = 'https://cors-anywhere.herokuapp.com/'; //Через CORS
-export const proxy = 'https://aoe2proxy.herokuapp.com/' //  Свой сервак
+export const proxy = 'https://aoe2proxy.herokuapp.com/' //  Свой сервак (нужно создать на heroku)
 export const errorMes = 'Ошибка запроса :('
 
 export const Urlpath = {
